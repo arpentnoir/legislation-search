d75f5f5b4f794939c8f8232d14e078142389efe2
arpentnoir <namlleps.drahcir@gmail.com>
2016-11-10 12:55:45 +1100

    Implement consolidated legislation search field
    main page now has a search field which filters the list of acts by title text. nothing complicated.
    
    test/ added with trivial passing test, just to check the CI pipeline is working.
    
    Resolves: #1, #2, #4, #6

--- public/js/app.js
+++ public/js/app.js
@@ -10,13 +10,9 @@ angular.module("legislationSearchApp", ['ngRoute'])
                     }
                 }
             })
-            .when("/new/contact", {
-                controller: "NewContactController",
-                templateUrl: "contact-form.html"
-            })
-            .when("/contact/:contactId", {
-                controller: "EditContactController",
-                templateUrl: "contact.html"
+            .when("/legislation/:actId", {
+                controller: "ViewActController",
+                templateUrl: "actView.html"
             })
             .otherwise({
                 redirectTo: "/"
@@ -31,86 +27,28 @@ angular.module("legislationSearchApp", ['ngRoute'])
                     alert("Error finding legislation.");
                 });
         }
-        this.createContact = function(contact) {
-            return $http.post("/contacts", contact).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error creating contact.");
-                });
-        }
-        this.getContact = function(contactId) {
-            var url = "/contacts/" + contactId;
+        this.getAct = function(actId) {
+            var url = "/legislation/" + actId;
             return $http.get(url).
                 then(function(response) {
                     return response;
                 }, function(response) {
-                    alert("Error finding this contact.");
-                });
-        }
-        this.editContact = function(contact) {
-            var url = "/contacts/" + contact._id;
-            console.log(contact._id);
-            return $http.put(url, contact).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error editing this contact.");
-                    console.log(response);
-                });
-        }
-        this.deleteContact = function(contactId) {
-            var url = "/contacts/" + contactId;
-            return $http.delete(url).
-                then(function(response) {
-                    return response;
-                }, function(response) {
-                    alert("Error deleting this contact.");
-                    console.log(response);
+                    alert("Error finding this act.");
                 });
         }
     })
     .controller("ListController", function(legislation, $scope) {
         $scope.legislation = legislation.data;
     })
-    .controller("NewContactController", function($scope, $location, Contacts) {
-        $scope.back = function() {
-            $location.path("#/");
-        }
-
-        $scope.saveContact = function(contact) {
-            Contacts.createContact(contact).then(function(doc) {
-                var contactUrl = "/contact/" + doc.data._id;
-                $location.path(contactUrl);
-            }, function(response) {
-                alert(response);
-            });
-        }
-    })
-    .controller("EditContactController", function($scope, $routeParams, Contacts) {
-        Contacts.getContact($routeParams.contactId).then(function(doc) {
-            $scope.contact = doc.data;
+    .controller("ViewActController", function($scope, $routeParams, Legislation) {
+        Legislation.getAct($routeParams.actId).then(function(doc) {
+            $scope.act = doc.data;
         }, function(response) {
             alert(response);
         });
 
-        $scope.toggleEdit = function() {
-            $scope.editMode = true;
-            $scope.contactFormUrl = "contact-form.html";
-        }
-
         $scope.back = function() {
             $scope.editMode = false;
             $scope.contactFormUrl = "";
-        }
-
-        $scope.saveContact = function(contact) {
-            Contacts.editContact(contact);
-            $scope.editMode = false;
-            $scope.contactFormUrl = "";
-        }
-
-        $scope.deleteContact = function(contactId) {
-            Contacts.deleteContact(contactId);
         }
     });
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
 
-var CONTACTS_COLLECTION = "contacts";
 var LEGISLATION_COLLECTION = "legislation";
 
 var app = express();
@@ -13,6 +12,7 @@ app.use(bodyParser.json({limit: '20Mb'}));
 
 var elasticsearch = require('elasticsearch');
 var connectionString = process.env.SEARCHBOX_SSL_URL;
+
 var client = new elasticsearch.Client({
     host: connectionString
 });
@@ -39,7 +39,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   });
 });
 
-// CONTACTS API ROUTES BELOW
+// LEGISLATION API ROUTES
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
@@ -47,37 +47,10 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
+/*  "/legislation"
+ *    GET: finds all legislation
+ *    POST: creates a new legislation record
  */
-
-app.get("/contacts", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
-    if (err) {
-      handleError(res, err.message, "Failed to get contacts.");
-    } else {
-      res.status(200).json(docs);
-    }
-  });
-});
-
-app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
-
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
-  }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
-});
 
 app.get("/legislation", function(req, res) {
   db.collection(LEGISLATION_COLLECTION).find({},{id:1, title:1}).toArray(function(err, docs) {
@@ -103,39 +76,39 @@ app.post("/legislation", function(req, res) {
   });
 });
 
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
+/*  "/legislation/:id"
+ *    GET: find legislation by id
+ *    PUT: update legislation by id
+ *    DELETE: deletes legislation by id
  */
 
-app.get("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+app.get("/legislation/:id", function(req, res) {
+  db.collection(LEGISLATION_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to get contact");
+      handleError(res, err.message, "Failed to get legislation");
     } else {
       res.status(200).json(doc);
     }
   });
 });
 
-app.put("/contacts/:id", function(req, res) {
+app.put("/legislation/:id", function(req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
 
-  db.collection(CONTACTS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(LEGISLATION_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
     if (err) {
-      handleError(res, err.message, "Failed to update contact");
+      handleError(res, err.message, "Failed to update legislation");
     } else {
       res.status(204).end();
     }
   });
 });
 
-app.delete("/contacts/:id", function(req, res) {
-  db.collection(CONTACTS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete("/legislation/:id", function(req, res) {
+  db.collection(LEGISLATION_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
     if (err) {
-      handleError(res, err.message, "Failed to delete contact");
+      handleError(res, err.message, "Failed to delete legislation");
     } else {
       res.status(204).end();
     }
